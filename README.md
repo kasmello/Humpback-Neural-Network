5edd8d285de211005b28d838e7b1c42060e5367e
kasmello <karmel.gandahusada@gmail.com>
2022-01-20 21:21:05 +0800

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Humpback-Neural-Network
+
+Readme file
+20/01/22
+
+(to fill out)
---
+++ main.py
@@ -0,0 +1,4 @@
+'''
+This is the main module of the Neural Network Program to automatically
+detect Humpback whales
+'''
