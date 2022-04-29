d204ffa68f7029f2aaf1d585d24b5de540fc84ab
kasmello <karmel.gandahusada@gmail.com>
2022-04-29 09:42:21 +0800

    migrated training and validation functions to NNfunctions

---
+++ NNfunctions.py
@@ -0,0 +1,87 @@
+import torch
+import wandb
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+from tqdm import tqdm
+from sklearn.metrics import classification_report
+
+def train_pretrained_nn(DATA,lr=0.001,optimizer=optim.AdamW,net=None,epochs=10,lbl='',\
+                loss_f=F.nll_loss, momentum=None):
+    name = str(net)
+    if len(str(net))>10:
+        name = lbl
+    wandb.init(project=name, name=f'lr={lr}',entity="kasmello")
+    if momentum:
+        optimizer = optimizer(net.parameters(),lr = lr,momentum = momentum)
+    else:
+        optimizer = optimizer(net.parameters(),lr = lr) #adamw algorithm
+    for epoch in tqdm(range(epochs)):
+        for batch in tqdm(DATA.all_training, leave = False):
+            net.train()
+            x,y = batch
+            net.zero_grad()
+             #sets gradients at 0 after each batch
+            output = F.log_softmax(net(x),dim=1)
+            #calculate how wrong we are
+            loss = loss_f(output,y)
+            loss.backward()#backward propagation
+            optimizer.step()
+        net.eval()
+        actual, pred = validate_model(net,loss)
+        if epoch == epochs-1:
+            return actual, pred
+
+def train_nn(DATA,lr=0.001,optimizer=optim.AdamW,net=None,epochs=10,lbl='',loss_f=F.nll_loss, momentum=None):
+    name = str(net)
+    if len(str(net))>10:
+        name = lbl
+    wandb.init(project=name, name=f'lr={lr}',entity="kasmello")
+    if momentum:
+        optimizer = optimizer(net.parameters(),lr = lr,momentum = momentum)
+    else:
+        optimizer = optimizer(net.parameters(),lr = lr) #adamw algorithm
+    for epoch in tqdm(range(epochs)):
+        for batch in tqdm(DATA.all_training, leave = False):
+            x,y = batch
+            net.zero_grad()
+             #sets gradients at 0 after each batch
+            if str(net)=='Net':
+                output = net(x.view(-1,224*224))
+            else:
+                output = net(x)
+            #calculate how wrong we are
+            loss = loss_f(output,y)
+            loss.backward()#backward propagation
+            optimizer.step()
+        actual, pred = validate_model(DATA,net,loss)
+        if epoch == epochs-1:
+            return actual, pred
+
+
+
+def validate_model(DATA,net,loss):
+    with torch.no_grad():
+        pred = []
+        actual = []
+
+        for batch_v in DATA.all_validation:
+            x,y = batch_v
+            if str(net)=='Net':
+                output = net(x.view(-1,224*224))
+            else:
+                output = net(x)
+
+            for idx, e in enumerate(output):
+                pred.append(torch.argmax(e))
+                actual.append(y[idx])
+        pred = DATA.inverse_encode(pred)
+        actual = DATA.inverse_encode(actual)
+        print('\n\n')
+        output = classification_report(actual,pred, output_dict = True)
+        accuracy = output['accuracy']
+        precision = output['weighted avg']['precision']
+        recall = output['weighted avg']['recall']
+        print({'Loss': loss, 'Validation Accuracy': accuracy, 'Wgt Precision': precision, 'Wgt Recall': recall})
+        wandb.log({'Loss': loss, 'Validation Accuracy': accuracy, 'Wgt Precision': precision, 'Wgt Recall': recall})
+        return actual,pred
--- main.py
+++ main.py
@@ -6,103 +6,20 @@ detect Humpback whales
 import os
 import cv2
 import torch
-import pickle
 import random
 import NNclasses
-import wandb
 import platform
-import matplotlib.pyplot as plt
 import numpy as np
 import torch.optim as optim
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
-from tqdm import tqdm
 from sklearn.metrics import classification_report
 from NNclasses import nn_data, Net, CNNet
+from NNfunctions import train_pretrained_nn, train_nn, validate_model
 
 
 DATA = None
-
-def train_pretrained_nn(lr=0.001,optimizer=optim.AdamW,net=None,epochs=10,lbl='',\
-                loss_f=F.nll_loss, momentum=None):
-    name = str(net)
-    if len(str(net))>10:
-        name = lbl
-    wandb.init(project=name, name=f'lr={lr}',entity="kasmello")
-    if momentum:
-        optimizer = optimizer(net.parameters(),lr = lr,momentum = momentum)
-    else:
-        optimizer = optimizer(net.parameters(),lr = lr) #adamw algorithm
-    for epoch in tqdm(range(epochs)):
-        for batch in tqdm(DATA.all_training, leave = False):
-            net.train()
-            x,y = batch
-            net.zero_grad()
-             #sets gradients at 0 after each batch
-            output = F.log_softmax(net(x),dim=1)
-            #calculate how wrong we are
-            loss = loss_f(output,y)
-            loss.backward()#backward propagation
-            optimizer.step()
-        net.eval()
-        actual, pred = validate_model(net,loss)
-        if epoch == epochs-1:
-            return actual, pred
-
-def train_nn(lr=0.001,optimizer=optim.AdamW,net=None,epochs=10,lbl='',loss_f=F.nll_loss, momentum=None):
-    name = str(net)
-    if len(str(net))>10:
-        name = lbl
-    wandb.init(project=name, name=f'lr={lr}',entity="kasmello")
-    if momentum:
-        optimizer = optimizer(net.parameters(),lr = lr,momentum = momentum)
-    else:
-        optimizer = optimizer(net.parameters(),lr = lr) #adamw algorithm
-    for epoch in tqdm(range(epochs)):
-        for batch in tqdm(DATA.all_training, leave = False):
-            x,y = batch
-            net.zero_grad()
-             #sets gradients at 0 after each batch
-            if str(net)=='Net':
-                output = net(x.view(-1,224*224))
-            else:
-                output = net(x)
-            #calculate how wrong we are
-            loss = loss_f(output,y)
-            loss.backward()#backward propagation
-            optimizer.step()
-        actual, pred = validate_model(net,loss)
-        if epoch == epochs-1:
-            return actual, pred
-
-
-
-def validate_model(net,loss):
-    with torch.no_grad():
-        pred = []
-        actual = []
-
-        for batch_v in DATA.all_validation:
-            x,y = batch_v
-            if str(net)=='Net':
-                output = net(x.view(-1,224*224))
-            else:
-                output = net(x)
-
-            for idx, e in enumerate(output):
-                pred.append(torch.argmax(e))
-                actual.append(y[idx])
-        pred = DATA.inverse_encode(pred)
-        actual = DATA.inverse_encode(actual)
-        print('\n\n')
-        output = classification_report(actual,pred, output_dict = True)
-        accuracy = output['accuracy']
-        precision = output['weighted avg']['precision']
-        recall = output['weighted avg']['recall']
-        print({'Loss': loss, 'Validation Accuracy': accuracy, 'Wgt Precision': precision, 'Wgt Recall': recall})
-        wandb.log({'Loss': loss, 'Validation Accuracy': accuracy, 'Wgt Precision': precision, 'Wgt Recall': recall})
-        return actual,pred
 
 if __name__ == '__main__':
     finished = False
@@ -197,11 +114,11 @@ if __name__ == '__main__':
                         input = png
 
         elif option[0] == '6':
-            actual, pred = train_nn(net = Net(), lr = 0.001)
+            actual, pred = train_nn(DATA=DATA,net = Net(), lr = 0.001)
             print(classification_report(actual,pred))
 
         elif option == '7':
-            actual, pred = train_nn(net = CNNet(), lr = 0.001)
+            actual, pred = train_nn(DATA=DATA,net = CNNet(), lr = 0.001)
             print(classification_report(actual,pred))
 
         elif option == '8':
@@ -215,7 +132,7 @@ if __name__ == '__main__':
             lr = 0.01
             momentum = 0.9
             epochs=10
-            actual, pred = train_pretrained_nn(lr=lr,optimizer=optim.SGD,net=model,epochs=epochs,lbl='ResNet18',\
+            actual, pred = train_pretrained_nn(DATA=DATA,lr=lr,optimizer=optim.SGD,net=model,epochs=epochs,lbl='ResNet18',\
                             loss_f=F.nll_loss, momentum=momentum)
             print(classification_report(actual,pred))
 
