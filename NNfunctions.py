b44b16a3d14ec560a595a8cc615096f3840e8153
kasmello <karmel.gandahusada@gmail.com>
2022-05-13 16:11:46 +0800

    replaced vit-pytorch with timm, added functionality to log images

--- NNfunctions.py
+++ NNfunctions.py
@@ -2,16 +2,18 @@ import torch
 import wandb
 import ssl
 from time import time
+import timm
 import torch.optim as optim
 import torch.nn as nn
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
 from tqdm import tqdm
 import torch.optim as optim
 import torch.nn as nn
 from vit_pytorch import ViT
 import torch.nn.functional as F
 import torchvision.models as models
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
 from NNclasses import Net, CNNet
 
 ssl._create_default_https_context = ssl._create_unverified_context
@@ -23,7 +25,14 @@ def extract_f1_score(DATA, dict):
     wandb.log({"F1-chart" : wandb.plot.bar(table, "Label", "F1-score",
                                 title="F1-chart")})
 
+def log_images(images,pred,actual):
+    images = [wandb.Image(image) for image in images]
+    data = [[pred[i],actual[i],images[i]] for i in range(len(images))]
+    table = wandb.Table(data=data, columns = ["Predicted", "Actual",'Spectogram'])
+    wandb.log({'Image Table': table})
 
+          
+    
 def train_pretrained_nn(DATA, lr=0.001, optimizer=optim.AdamW, net=None, epochs=5, lbl='',
                         loss_f=F.nll_loss, momentum=None, wd=0):
     name = str(net)
@@ -82,8 +91,9 @@ def predict(data,net, num_batches=999999999):
     with torch.no_grad():
         pred = []
         actual = []
-        i = 0
-        for batch_v in data:
+        for i, batch_v in enumerate(data):
+            predicted = []
+            label = []
             x, y = batch_v
             if str(net) == 'Net':
                 output = net(x.view(-1, 224 * 224))
@@ -91,15 +101,16 @@ def predict(data,net, num_batches=999999999):
                 output = net(x)
 
             for idx, e in enumerate(output):
-                pred.append(torch.argmax(e))
-                actual.append(y[idx])
-            i += 1
-            if i == num_batches:
-                break
-        return pred, actual
+                predicted.append(torch.argmax(e))
+                label.append(y[idx])
+            pred.extend(predicted)
+            actual.extend(label)
+            if i == num_batches-1 or i == len(data)-1:
+                return x, predicted, label, pred, actual
+        
 
 def check_training_accuracy(DATA,net):
-    pred, actual = predict(DATA.all_training, net, 125)
+    images, predicted, label, pred, actual = predict(DATA.all_training, net, 125)
     pred = DATA.inverse_encode(pred)
     actual = DATA.inverse_encode(actual)
     print('\n\n')
@@ -114,7 +125,9 @@ def check_training_accuracy(DATA,net):
 
 
 def validate_model(DATA, net, loss, final_layer):
-    pred, actual = predict(DATA.all_validation,net)
+    images, predicted, label, pred, actual = predict(DATA.all_validation,net)
+    predicted = DATA.inverse_encode(predicted)
+    label = DATA.inverse_encode(label)
     pred = DATA.inverse_encode(pred)
     actual = DATA.inverse_encode(actual)
     print('\n\n')
@@ -127,8 +140,14 @@ def validate_model(DATA, net, loss, final_layer):
     print(result_dict)
     wandb.log(result_dict)
     if final_layer:
+        log_images(images,predicted,label)
         extract_f1_score(DATA, output)
         print(classification_report(actual, pred))
+        cm = confusion_matrix(actual,pred)
+        disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+        disp.plot()
+        plt.show()
+        
 
 def run_model(DATA,net,lr,wd,epochs,momentum):
     if net == 'net':
@@ -156,17 +175,18 @@ def run_model(DATA,net,lr,wd,epochs,momentum):
         train_pretrained_nn(DATA=DATA, lr=lr, wd=wd, epochs=epochs, optimizer=optim.SGD, net=model, lbl='VGG16',
                                             loss_f=F.nll_loss, momentum=momentum)
     elif net == 'vit':
-        v = ViT(
-            image_size=224,
-            patch_size=16,
-            num_classes=23,
-            dim=1024,
-            depth=6,
-            heads=8,
-            mlp_dim=2048,
-            dropout=0,
-            emb_dropout=0,
-            channels=1
-        )
-        actual, pred = train_pretrained_nn(DATA=DATA, lr=lr, wd=wd, epochs=epochs, optimizer=optim.SGD, net=v, lbl='ViT',
+        v = timm.create_model('vit_base_patch16_224',pretrained=True, num_classes=23, in_chans=1)
+        # v = ViT(
+        #     image_size=224,
+        #     patch_size=16,
+        #     num_classes=23,
+        #     dim=1024,
+        #     depth=6,
+        #     heads=16,
+        #     mlp_dim=2048,
+        #     dropout=0.1,
+        #     emb_dropout=0.1,
+        #     channels=1
+        # )
+        train_pretrained_nn(DATA=DATA, lr=lr, wd=wd, epochs=epochs, optimizer=optim.SGD, net=v, lbl='ViT',
                                             loss_f=F.nll_loss, momentum=momentum)
