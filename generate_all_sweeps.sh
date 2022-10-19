b3e88a7621f8ee0dc93964e3de14c0a7d11031e6
kasmello <karmel.gandahusada@gmail.com>
2022-10-19 20:34:32 +0800

    updated so user can set their own name

--- generate_all_sweeps.sh
+++ generate_all_sweeps.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-all_nets=(custom vgg16 resnet18 vit)
+all_nets=(vgg16 resnet18 vit)
+#var=$(date '+%d-%m-%Y')
+echo Hello, what would you like to call this project?
+read projname
 rm sweeps.txt
 for n in ${all_nets[@]}; do
+  echo $n
   sweep_str=$n.yaml
+  cp "$sweep_str" "$sweep_str.temp"
+  echo "project: Sweep_$projname" > "$sweep_str"
+  cat "$sweep_str.temp" |tail -n+2>> "$sweep_str"
   wandb sweep $sweep_str >.temp 2>&1
   tail -1 .temp > .temp2 2>&1
   line=$(cat .temp2)
-  echo ${line:29:43} >> sweeps.txt
-
+  echo ${line:29:54} >> sweeps.txt
+rm "$sweep_str.temp"
 
 done
 rm .temp
