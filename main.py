f7fa3eb195c6a236553cc3579855751bc80b0568
kasmello <karmel.gandahusada@gmail.com>
2022-11-19 12:50:50 +0800

    fix

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ if __name__ == '__main__':
             name='vit-base'
             optimm='adamw'
             lr_decay = 'cosineANW'
-            run_model(DATA,name,lr,wd,momentum,epochs, True, True, optimm, lr_decay, True)
+            run_model(DATA,name,lr,wd,momentum,epochs, True, True, optimm, lr_decay)
 
         elif option == '3':
             DATA.test_transform()
